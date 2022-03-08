4a7501dd86809e00be2c5602381f3eac137eeee9
a_zawada <annazawada1986@gmail.com>
2022-03-08 18:25:05 +0100

    class game_list created game tables created function game ready

--- main.py
+++ main.py
@@ -39,8 +39,10 @@ class game_list:
         tab2 = 2 * tab2
         return tab2
 
-#board = game_list(file_words,4)
-#board.create_tab()
+#board = game_list(file_words,3)
+#a = board.create_tab()
+#random.shuffle(a)
+#print(a)
 
 def x_board(num_cards):
     x_b = pd.DataFrame(np.array([["X" for i in range(0, num_cards)], ["X" for i in range(0, num_cards)]]), index=["A", "B"],
@@ -49,9 +51,53 @@ def x_board(num_cards):
 
 def word_board(num_cards):
     w_b = game_list(file_words,num_cards)
-    random.shuffle(w_b.create_tab())
-    w_b2 = pd.DataFrame(np.array([[w_b.create_tab()[i] for i in range(0, num_cards)], [w_b.create_tab()[i] for i in range(num_cards, 2*num_cards)]]), index=["A", "B"],
+    w_b3 = w_b.create_tab()
+    random.shuffle(w_b3)
+    w_b2 = pd.DataFrame(np.array([[w_b3[i] for i in range(0, num_cards)], [w_b3[i] for i in range(num_cards, 2*num_cards)]]), index=["A", "B"],
                       columns=[i for i in range(1, num_cards+1)])
     return w_b2
 
-print(word_board(6))
+#print(word_board(6))
+#supporting_table = x_board()
+
+def game(x_board,word_board,num_quess,num_cards):
+    if num_cards == 4:
+        level = 'EASY'
+    else:
+        level = "DIFFICULT"
+
+    win = False
+    k = 0
+
+    while num_quess > 0:
+        if num_cards == 0:
+            win = True
+            num_quess = 0
+            break
+        else:
+            k += 1
+            print("Level: ",level,"\n Chances: ", num_quess)
+            print(x_board)
+            row_1 = str(input("Enter the row: \n"))
+            col_1 = int(input("Enter the col: \n"))
+            x_board.at[row_1, col_1] = word_board.at[row_1, col_1]
+            print(x_board)
+            row_2 = str(input("Enter the row: \n"))
+            col_2 = int(input("Enter the col: \n"))
+            x_board.at[row_2, col_2] = word_board.at[row_2, col_2]
+            print(x_board)
+            if x_board.at[row_1, col_1] != x_board.at[row_2, col_2]:
+                x_board.at[row_1, col_1] = 'X'
+                x_board.at[row_2, col_2] = 'X'
+                num_quess -= 1
+            else:
+                num_cards -= 1
+                num_quess -= 1
+
+    return win,k
+
+#num_cards = 3
+#num_quess = 6
+#vx_board = x_board(num_cards)
+#vword_board = word_board(num_cards)
+#game(vx_board,vword_board,num_quess,num_cards)
